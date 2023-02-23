fc680da31bc9f07fe07060a606a52474d83360fa
VanLamCS <levanlam7602@gmail.com>
2023-02-23 07:45:41 +0700

    Fix route get a user by Id

--- server/routes/RoutesDefine.php
+++ server/routes/RoutesDefine.php
@@ -3,5 +3,5 @@
 $routes = [
     array('method' => 'POST', 'route' => 'api/user/login', 'regex' => '/^api\/user\/login$/', 'controller' => 'User', 'action' => 'Login'),
     array('method' => 'POST', 'route' => 'api/user/register', 'regex' => '/^api\/user\/register$/', 'controller' => 'User', 'action' => 'Register'),
-    array('method' => 'GET', 'route' => 'api/user/:id', 'regex' => '/^api\/user\/[0-9a-fA-F]{24}$/', 'controller' => 'User', 'action' => 'GetUserById')
+    array('method' => 'GET', 'route' => 'api/user/:id', 'regex' => '/^api\/user\/[0-9]+$/', 'controller' => 'User', 'action' => 'GetUserById')
 ];
